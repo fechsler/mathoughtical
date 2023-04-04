50084e557816f5b0e6fe2f311b1330dab0028849
m-lm <31556425+m-lm@users.noreply.github.com>
2023-04-04 01:06:58 -0500

    rewrite addition problem function

--- src/js/problem_generator.js
+++ src/js/problem_generator.js
@@ -9,11 +9,28 @@ function randChoice(choices) {
 	return choices[index];
 }
 
-function generateAddProblem(maxDigits = 1) {
+function generateAddProblem(maxDigits = 3) {
 	const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	var problem = String(randChoice(nums)) + " × " + String(randChoice(nums)) + " = ";
+	num1 = "";
+	num2 = "";
+	for (i = 0; i < maxDigits; i++) {
+		num1 += String(randChoice(nums));
+		num2 += String(randChoice(nums));
+		// Remove leading zeros from multidigit numbers
+		if (maxDigits > 1) {
+			if (num1[0] == "0") {
+				num1 = num1.replace(num1[0], "");
+			}
+			if (num2[0] == "0") {
+				num2 = num1.replace(num1[0], "");
+			}
+		}
+	}
+	var problem = num1 + " + " + num2 + " = ";
 	document.getElementById("name-header").addEventListener("click", generateProblem(problem));
 }
+
+// multiplication sign = ×
 
 generateAddProblem();
 
